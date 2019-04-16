3220481220bc036359d439a951132119fdefcf78
Somers Matthews <somersbmatthews@gmail.com>
2019-04-16 11:53:44 -0500

    first commit

---
+++ elixir/ds/array_rotation.ex
@@ -0,0 +1,15 @@
+# Program for array rotation
+# Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
+# array [1, 2, 3, 4, 5, 6, 7] rotates to array [3, 4, 5, 6, 7, 1, 2]
+
+defmodule arrayRotation do
+    def rotateByOne do
+
+    end
+    def rotateByN do
+
+    end
+    def main do
+    
+    end
+end 
