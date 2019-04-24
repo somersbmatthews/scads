6d3f5564de6d2875f510a076383209a3fbe337c5
Somers Matthews <somersbmatthews@gmail.com>
2019-04-24 18:58:56 -0500

    added commented sample output of tail recursion

--- elixir/ds/array_rotation.ex
+++ elixir/ds/array_rotation.ex
@@ -2,10 +2,14 @@
 # Write a function rotate(ar[], d, n) that rotates arr[] of size n by d elements.
 # array [1, 2, 3, 4, 5, 6, 7] rotates to array [3, 4, 5, 6, 7, 1, 2]
 
-defmodule arrayRotation do
-    defp leftRotateByN do
+defmodule ArrayRotation do
+    def leftRotateByN do
     # for (int i = 0; i < d; i++) 
     #     leftRotatebyOne(arr, n);
+
+        for n <- 0..10 do
+            IO.puts "hello "
+        end
     end
     defp leftRotateByOne do
     # for x <- 0..10, do: IO.puts x
@@ -24,3 +28,15 @@ defmodule arrayRotation do
     # rotate.printArray(arr, 7); 
     end
 end 
+
+# [head | tail] = [1, 2, 3]
+# head # 1
+# tail # [2, 3]
+
+# [head | tail] = tail
+# head # 2
+# tail # [3]
+
+# [head | tail] = tail
+# head # 3
+# tail # []
